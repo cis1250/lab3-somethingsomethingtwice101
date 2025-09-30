5a655c2b4386aa0b394d5cdf0cfdb816f97c1405
Ethan Yan <230450599+somethingsomethingtwice101@users.noreply.github.com>
2025-09-30 11:27:16 -0400

    Fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
 # Fibonacci Sequence Exercise
-# TODO: (Read detailed instructions in the Readme file)
-# Prompt the user for the number of terms.
-# Validate that the input is a positive integer.
-# Use a for loop to print the Fibonacci sequence up to that many terms.
+nums = [0, 1]
+terms = -1
+while True:
+    try:
+        terms = int(input("Number of terms in the fibonacci sequence: "))
+    except ValueError:
+        print("Input is not a positive integer.")
+        continue
+        
+    if (terms > 0):
+        break
+    print("Please enter a positive integer.")
+
+for i in range(terms):
+    if (i < len(nums)):
+        print(nums[i])
+        continue
+    
+    s = sum(nums)
+    nums[0] = nums[1]
+    nums[1] = s
+    print(s)
